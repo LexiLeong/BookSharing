5d45716ebc84cd9fdace8deff18cd75e4e6f0036
ZoeDong <60535303+ZoeDong@users.noreply.github.com>
2021-04-01 01:33:46 +0800

    首页已有图书显示

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -9,17 +9,43 @@ Page({
   data: {
     bookList:[]
   },
-
+ /**
+   * 生命周期函数--监听页面加载
+   */
+  //页面开始加载就会触发
+  onLoad: function (options) {
+    this.getData();
+  },
   getData(){
-    let getDataVar = this
+    
+    let that = this
+    var arr=[]
     db.collection('releaseInfo').where({})
     .get({
       success(res) { // res.data 是一个包含集合中有权限访问的所有记录的数据，不超过 20 条
-        console.log("success",res);
-        console.log("success",res.data[0].发布3);
-
-        getDataVar.setData({
-          bookList:res.data
+        for(var i=0;i<Object.keys(res.data).length;i++){//总共有多少个用户
+          for(var j=2;j<Object.keys(res.data[i]).length;j++){//每个用户发布了多少书
+            var bookName=Object.keys(res.data[i])[j];
+            var _={
+              _bookName:Object.keys(res.data[i])[j],
+              _author:res.data[i][bookName].author,
+              _description:res.data[i][bookName].description,
+              _picid:res.data[i][bookName].picid
+            }
+            arr.push(_);
+            //console.log("书名：",_.bookName,"作者：",_._author,"描述：",_._description,"图片id：",_._picid);
+            var a=arr;
+            wx.getSystemInfo({
+              success: function (res) {
+                that.setData({
+                  bookList: a,
+                })
+              }
+            })
+          }
+        }
+        that.setData({
+          bookList,
         })
       },
       fail(res){
@@ -28,25 +54,26 @@ Page({
     })
   },
 
-
-
-
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  //页面开始加载就会触发
-  onLoad: function (options) {
-
-  },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
 
   },
-
+  test()
+  {
+    return new Promise((resolve, reject)=>{
+    var a=this.data.arr;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          //btn
+          bookList: a,
+        })
+      }})
+      console.log(a);
+      setTimeout(()=>{resolve(this.data.arr)},1500)
+  })},
   /**
    * 生命周期函数--监听页面显示
    */
@@ -89,3 +116,10 @@ Page({
 
   }
 })
+
+
+
+            // console.log("书名：",bookName,"作者：",res.data[i][bookName].author,"描述：",res.data[i][bookName].description,"图片id：",res.data[i][bookName].picid);
+            // console.log(res.data[i][bookName].author);
+            // console.log(res.data[i][bookName].description);
+            // console.log(res.data[i][bookName].picid);

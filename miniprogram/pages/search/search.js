4ad39770569cea1e7ab0181ac5902bf238906da3
YSS349 <81564485+YSS349@users.noreply.github.com>
2021-04-03 13:20:44 +0800

    Update search.js

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -8,40 +8,9 @@ Page({
    */
   data: {
     bookList:[],
-    inputMsg:'没',
-    evaluate_contant: ['评价:'],
-    stars: [0, 1, 2, 3, 4],
-    normalSrc: './star_default.png',
-    selectedSrc: './star_full.png',
-    halfSrc: './star_half.png',
-    score: 0,
-    scores: [0],
+    inputMsg:'没'
   },
-// 提交事件
-submit_evaluate: function () {
-  console.log('评价得分' + this.data.scores)
-},
-//点击左边,半颗星
-selectLeft: function (e) {
-  var score = e.currentTarget.dataset.score
-  if (this.data.score == 0.5 && e.currentTarget.dataset.score == 0.5) {
-    score = 0;
-  }
-  this.data.scores[e.currentTarget.dataset.idx] = score,
-    this.setData({
-      scores: this.data.scores,
-      score: score
-    })
-},
-//点击右边,整颗星
-selectRight: function (e) {
-  var score = e.currentTarget.dataset.score
-  this.data.scores[e.currentTarget.dataset.idx] = score,
-    this.setData({
-      scores: this.data.scores,
-      score: score
-    })
-},
+
   /**
    * 生命周期函数--监听页面加载
    */
